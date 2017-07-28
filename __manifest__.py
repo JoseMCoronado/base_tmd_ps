5e8a09b5da875f675f7b0d400ca31658c40a3dd5
Jose M. Coronado <j.coronado.personal@gmail.com>
2017-07-28 13:45:09 -0700

    Initial Commit

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'TMD Holdings Base Package',
+    'category': 'Sale',
+    'summary': 'Custom',
+    'version': '1.0',
+    'description': """
+Intial configurations & customizations to the base database
+        """,
+    'depends': ['base','stock'],
+    'data': [
+        'data/inventory_locations.xml',
+        #'data/ir_model_fields.xml',
+        #'data/ir_actions_act_window.xml',
+        #'data/ir_actions_server.xml',
+        #'data/ir_ui_view.xml',
+        #'data/ir_ui_menu.xml',
+        #'data/base_automation.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
